cd89e82d1579388ffe269e33c56bcbc810404c77
Sotirios Chortogiannos <chortogiannoss@gmail.com>
2026-04-11 09:30:37 -0400

    Remove V3 eval artifacts from operational polish

--- src/lib/engine_v3_eval/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "@/lib/engine_v3_eval/types";
-export * from "@/lib/engine_v3_eval/scenarioMatrix";
-export * from "@/lib/engine_v3_eval/normalize";
-export * from "@/lib/engine_v3_eval/evaluate";
-export * from "@/lib/engine_v3_eval/report";
